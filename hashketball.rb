526af68fb3e126c17e1b74c875efc0e4883ed2e4
Robert <r_lee94@yahoo.com>
2019-02-21 19:25:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,10 @@
 # Write your code here!
-
+def points_of_player(player_name, game)
+  game.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
+      if player_hash[:name] == player_name
+        return player_hash[:stats][:points]
+      end
 
 
 
